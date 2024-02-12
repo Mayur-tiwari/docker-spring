dad0b32abb01323ee21faaba8c1b69ecf6341e8b
Mayur-tiwari <mt82812@gmail.com>
2024-02-13 01:36:29 +0530

    change

---
+++ staragile1/src/main/java/com/example/staragile1/Staragile1Application.java
@@ -0,0 +1,13 @@
+package com.example.staragile1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Staragile1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Staragile1Application.class, args);
+	}
+
+}
---
+++ staragile1/src/test/java/com/example/staragile1/Staragile1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staragile1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Staragile1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
