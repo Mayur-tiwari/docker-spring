67e0984e15d38fc59e9da05b6bdcaa7bd546e1e0
Mayur-tiwari <mt82812@gmail.com>
2024-02-13 11:55:22 +0530

    h

---
+++ staragile2/src/main/java/com/example/staragile2/Staragile2Application.java
@@ -0,0 +1,13 @@
+package com.example.staragile2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Staragile2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Staragile2Application.class, args);
+	}
+
+}
---
+++ staragile2/src/test/java/com/example/staragile2/Staragile2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staragile2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Staragile2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
