137d66bb51ec02617833d5ffd25a5a691f0546c6
Mayur-tiwari <mt82812@gmail.com>
2024-03-03 16:44:42 +0530

    added

---
+++ src/main/java/com/star/agile/assignment/springbootdemoo/SpringbootDemooApplication.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemooApplication.class, args);
+	}
+
+}
