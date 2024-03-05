447b52156c3d471b1897de5ab7c67f1d14b7912f
Mayur-tiwari <mt82812@gmail.com>
2024-03-05 14:45:07 +0530

    added

---
+++ src/test/java/com/example/springbootdamo/SpringbootDamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
